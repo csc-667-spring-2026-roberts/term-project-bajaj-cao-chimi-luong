f16ac5264f2687d61206b35f4d31d7b69f4c7756
Kevin Luong <notepadkevin@icloud.com>
2026-05-02 12:47:58 -0700

    4/27 updates for render

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -9,6 +9,9 @@ if (connectionString === undefined) {
 }
 
 const pgp = pgPromise();
-const connection = pgp(connectionString);
+const connection = pgp({
+  connectionString,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+});
 
 export default connection;
